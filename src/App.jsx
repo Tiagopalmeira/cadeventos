c967bad83232917b2ede616feb320e0636acd58a
Tiago Santana Palmeira <125421932+Tiagopalmeira@users.noreply.github.com>
2025-01-29 17:58:28 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,264 @@
-import express from 'express';
-import cors from 'cors';
-import { PrismaClient } from '@prisma/client';  // Importando o PrismaClient
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-const app = express();
-const port = 3000;
+const CriarEvento = () => {
+  const [novoEvento, setNovoEvento] = useState({
+    title: "",
+    startDate: "",
+    startTime: "",
+    endDate: "",
+    endTime: "",
+    descricao: "",
+    uniforme: "",
+    instrutor: "",
+  });
+  const [eventos, setEventos] = useState([]);
+  const [showModal, setShowModal] = useState(false); // Controla a visibilidade do modal
+  const [eventoParaExcluir, setEventoParaExcluir] = useState(null); // Armazena o evento selecionado para excluir
+  const [senha, setSenha] = useState(""); // Armazena a senha inserida
 
-const prisma = new PrismaClient();  // Instanciando o PrismaClient
+  const senhaCorreta = "EDN123"; // A senha para confirmar a exclusão
 
-// Configurando CORS para permitir todas as origens
-app.use(cors({
-  origin: '*', // Permite todas as origens. Para permitir apenas uma origem específica, substitua '*' pela URL da origem.
-}));
+  // Função para carregar eventos do servidor
+  const loadEventos = async () => {
+    const response = await fetch("https://apidata-nerdflbqz-tiagopalmeiras-projects.vercel.app/eventos");
+    const data = await response.json();
+    setEventos(data);
+  };
 
-app.use(express.json());  // Para poder lidar com JSON
+  useEffect(() => {
+    loadEventos();
+  }, []);
 
-// Endpoint para obter os eventos
-app.get('/eventos', async (req, res) => {
-  try {
-    const eventos = await prisma.cadastro.findMany(); // Buscando todos os eventos
-    res.json(eventos); // Retorna todos os eventos
-  } catch (err) {
-    console.error('Erro ao buscar eventos', err);
-    res.status(500).json({ error: 'Erro ao buscar eventos' });
-  }
-});
+  // Função para excluir um evento
+  const handleDeleteEvent = async () => {
+    if (senha === senhaCorreta) {
+      try {
+        const response = await fetch(
+          `https://apidata-nerdflbqz-tiagopalmeiras-projects.vercel.app/eventos/${eventoParaExcluir.id}`,
+          {
+            method: "DELETE",
+          }
+        );
 
-// Endpoint para adicionar um novo evento
-app.post('/eventos', async (req, res) => {
-  const { title, startDate, startTime, endDate, endTime, descricao, uniforme, instrutor } = req.body;
+        if (response.ok) {
+          console.log("Evento excluído com sucesso!");
+          setEventos(
+            eventos.filter((evento) => evento.id !== eventoParaExcluir.id)
+          );
+          setShowModal(false); // Fecha o modal após excluir
+        } else {
+          console.error("Erro ao excluir evento");
+        }
+      } catch (error) {
+        console.error("Erro ao excluir evento:", error);
+      }
+    } else {
+      alert("Senha incorreta!"); // Alerta se a senha estiver incorreta
+    }
+  };
 
-  try {
-    const novoEvento = await prisma.cadastro.create({
-      data: {
-        title,
-        startDate: new Date(startDate), // Convertendo para Date
-        startTime,
-        endDate: new Date(endDate),     // Convertendo para Date
-        endTime,
-        descricao,
-        uniforme,
-        instrutor,
-      },
+  // Função para abrir o modal de exclusão
+  const openDeleteModal = (evento) => {
+    setEventoParaExcluir(evento);
+    setShowModal(true);
+  };
+
+  // Função para lidar com a mudança da senha
+  const handlePasswordChange = (e) => {
+    setSenha(e.target.value);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNovoEvento({
+      ...novoEvento,
+      [name]: value,
     });
-    res.status(201).json(novoEvento); // Retorna o evento inserido
-  } catch (err) {
-    console.error('Erro ao adicionar evento', err);
-    res.status(500).json({ error: 'Erro ao adicionar evento' });
-  }
-});
+  };
 
-// Endpoint para excluir um evento
-app.delete('/eventos/:id', async (req, res) => {
-  const { id } = req.params;
-  
-  try {
-    const eventoExcluido = await prisma.cadastro.delete({
-      where: { id: parseInt(id) },
-    });
-    
-    res.status(200).json({ message: 'Evento excluído com sucesso' }); // Retorna sucesso
-  } catch (err) {
-    console.error('Erro ao excluir evento', err);
-    res.status(500).json({ error: 'Erro ao excluir evento' });
-  }
-});
+  const handleSubmit = async () => {
+    const newEvent = {
+      title: novoEvento.title,
+      start: `${novoEvento.startDate}T${novoEvento.startTime}`,
+      end: `${novoEvento.endDate}T${novoEvento.endTime}`,
+      descricao: novoEvento.descricao,
+      uniforme: novoEvento.uniforme,
+      instrutor: novoEvento.instrutor,
+    };
 
-app.listen(port, () => {
-  console.log(`API rodando na porta ${port}`);
-});
+    try {
+      const response = await fetch("https://apidata-nerdflbqz-tiagopalmeiras-projects.vercel.app/eventos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newEvent),
+      });
+
+      if (response.ok) {
+        console.log("Evento adicionado com sucesso!");
+        setNovoEvento({
+          title: "",
+          startDate: "",
+          startTime: "",
+          endDate: "",
+          endTime: "",
+          descricao: "",
+          uniforme: "",
+          instrutor: "",
+        });
+        loadEventos(); // Recarrega os eventos
+      } else {
+        console.error("Erro ao adicionar evento");
+      }
+    } catch (error) {
+      console.error("Erro ao enviar evento:", error);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">Criar Evento</h1>
+
+      {/* Formulário de Criação de Evento */}
+      <div className="form-container">
+        <div className="input-group">
+          <label className="label">Título:</label>
+          <input
+            type="text"
+            name="title"
+            value={novoEvento.title}
+            onChange={handleChange}
+            className="input"
+          />
+        </div>
+
+        <div className="input-group-double">
+          <div className="input-group">
+            <label className="label">Data de Início:</label>
+            <input
+              type="date"
+              name="startDate"
+              value={novoEvento.startDate}
+              onChange={handleChange}
+              className="input"
+            />
+          </div>
+          <div className="input-group">
+            <label className="label">Hora de Início:</label>
+            <input
+              type="time"
+              name="startTime"
+              value={novoEvento.startTime}
+              onChange={handleChange}
+              className="input"
+            />
+          </div>
+        </div>
+
+        <div className="input-group-double">
+          <div className="input-group">
+            <label className="label">Data de Término:</label>
+            <input
+              type="date"
+              name="endDate"
+              value={novoEvento.endDate}
+              onChange={handleChange}
+              className="input"
+            />
+          </div>
+          <div className="input-group">
+            <label className="label">Hora de Término:</label>
+            <input
+              type="time"
+              name="endTime"
+              value={novoEvento.endTime}
+              onChange={handleChange}
+              className="input"
+            />
+          </div>
+        </div>
+
+        <div className="input-group">
+          <label className="label">Descrição:</label>
+          <textarea
+            name="descricao"
+            value={novoEvento.descricao}
+            onChange={handleChange}
+            className="input"
+          ></textarea>
+        </div>
+
+        <div className="input-group">
+          <label className="label">Uniforme:</label>
+          <input
+            type="text"
+            name="uniforme"
+            value={novoEvento.uniforme}
+            onChange={handleChange}
+            className="input"
+          />
+        </div>
+
+        <div className="input-group">
+          <label className="label">Instrutor:</label>
+          <input
+            type="text"
+            name="instrutor"
+            value={novoEvento.instrutor}
+            onChange={handleChange}
+            className="input"
+          />
+        </div>
+
+        <button onClick={handleSubmit} className="button primary">
+          Adicionar Evento
+        </button>
+      </div>
+
+      {/* Lista de Eventos */}
+      <div className="events-container">
+        <h2 className="subtitle">Eventos Adicionados</h2>
+        <ul>
+          {eventos.map((evento) => (
+            <li key={evento.id}>
+              <div>{evento.title}</div>
+              <div>{evento.descricao}</div>
+              <button onClick={() => openDeleteModal(evento)}>Excluir</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Modal de Confirmação de Exclusão */}
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>Confirmar Exclusão</h2>
+            <p>Digite a senha para excluir este evento:</p>
+            <input
+              type="password"
+              value={senha}
+              onChange={handlePasswordChange}
+              placeholder="Digite a senha"
+            />
+            <div className="modal-actions">
+              <button onClick={handleDeleteEvent} className="button primary">
+                Confirmar
+              </button>
+              <button
+                onClick={() => setShowModal(false)}
+                className="button secondary"
+              >
+                Cancelar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CriarEvento;
